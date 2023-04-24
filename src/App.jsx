98900240c4d9826a782b9e4adcf47c7a008459db
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-04-24 14:51:43 +0700

    api will now update state with setResults

--- src/App.jsx
+++ src/App.jsx
@@ -9,60 +9,6 @@ function App() {
   const [results, setResults] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
 
-  // data
-  let data = [
-    {
-      title: 'JS testing',
-      description: 'The best JavaScript tutorials in the galaxy!',
-      url: 'www.testing.com',
-      links: [
-        {
-          title: 'JS for Beginner tests',
-          url: 'https://www.w3schools.com/js',
-        },
-        {
-          title: 'JS for the Web',
-          url: 'https://www.w3schools.com/js',
-        },
-      ],
-    },
-    {
-      title: 'Tutorial - W3School',
-      description:
-        'Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL...',
-      url: 'developer.mozilla.org',
-      links: [
-        {
-          title: 'JS Introduction',
-          url: 'https://www.w3schools.com/js/js_intro.asp',
-        },
-        {
-          title: 'JS Functions',
-          url: 'https://www.w3schools.com/js/js_functions.asp',
-        },
-      ],
-    },
-    {
-      title: 'JavaScript | MDN',
-      description:
-        'JavaScript is the programming language of the Web. JavaScript is easy to learn. This tutorial will teach you JavaScript from basic to advanced. Start learning ...',
-      url: 'developer.mozilla.org',
-      links: [
-        {
-          title: 'JavaScript Operator',
-          url: 'https://www.w3schools.com/js/js_examples.asp',
-        },
-        {
-          title: 'JavaScript Code',
-          url: 'https://www.w3schools.com/js/js_functions.asp',
-        },
-        {
-          title: 'JavaScript Meaning',
-          url: 'https://www.w3schools.com/js/js_functions.asp',
-        },
-      ],
-    },
-  ]
   // function
   //  takes a string parameter then returns an array from the data array that contains only results where the string parameter can be found in either the title, description or url of the element.
   function setValue(str) {
@@ -72,47 +18,28 @@ function App() {
     e.preventDefault()
     let matchingResults = []
 
-    // access api
-    const apiSearch = async () => {
-      let searchResult = await axios.get(
-        'https://project-google-search-api-demo.herokuapp.com/results',
-        {
-          params: {
-            search: 'javascript',
-          },
-        }
-      )
-    }
-
-    console.log('testing api' + apiSearch())
-
-    // data.map((result, index) => {
-    //   let resultsArray = []
-    //   if (result.title.toLowerCase().includes(str.toLowerCase())) {
-    //     console.log(result)
-    //     // setResults(results.p)
-
-    //     results.push(result)
-    //     console.log(matchingResults)
-    //     console.log('matching results has been updated')
-    //   }
-    //   console.log(results)
-    //   setResults(results)
-    //   // return resultsArray
-    // })
-
-    const filteredResults = data.filter((result) =>
-      result.title.toLowerCase().includes(str.toLowerCase())
-    )
-    setResults(filteredResults)
-
     // if search bar is empty dont show any results
     if (str == '') {
       let emptyArr = []
       setResults(emptyArr)
     }
-    console.log(filteredResults)
+
     console.log(results)
+    apiSearch()
+  }
+
+  // access api
+  const apiSearch = async () => {
+    let searchResult = await axios.get(
+      'https://project-google-search-api-demo.herokuapp.com/results',
+      {
+        params: {
+          search: 'mozilla',
+        },
+      }
+    )
+    console.log(searchResult.data)
+    setResults(searchResult.data)
   }
   // return
   return (
